b2884368f128d3b701e5ee09800b0974f818c7b0
Gowri Jagadeesh <gowrimj2006@gmail.com>
2018-05-26 19:51:54 -0700

    implementing part 3

--- Project-Part-3-Start/Gruntfile.js
+++ Project-Part-3-Start/Gruntfile.js
@@ -6,56 +6,56 @@
   "grunt responsive_images" re-processes images without removing the old ones
 */
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    responsive_images: {
-      dev: {
-        options: {
-          engine: 'im',
-          sizes: [{
-            /* Change these */
-            width: 1600,
-            suffix: '_large_2x',
-            quality: 30
-          }]
-        },
+    grunt.initConfig({
+        responsive_images: {
+            dev: {
+                options: {
+                    //engine: 'im',
+                    sizes: [{
+                            /* Change these */
+                            width: 800,
+                            suffix: '_1x',
+                            quality: 30
+                        }]
+                },
 
-        /*
-        You don't need to change this part if you don't change
-        the directory structure.
-        */
-        files: [{
-          expand: true,
-          src: ['*.{gif,jpg,png}'],
-          cwd: 'images_src/',
-          dest: 'images/'
+                /*
+                You don't need to change this part if you don't change
+                the directory structure.
+                */
+                files: [{
+                    expand: true,
+                    src: ['*.{gif,jpg,png}'],
+                    cwd: 'images_src/',
+                    dest: 'images/'
         }]
-      }
-    },
-
-    /* Clear out the images directory if it exists */
-    clean: {
-      dev: {
-        src: ['images'],
-      },
-    },
-
-    /* Generate the images directory if it is missing */
-    mkdir: {
-      dev: {
-        options: {
-          create: ['images']
+            }
         },
-      },
-    },
 
-  });
+        /* Clear out the images directory if it exists */
+        clean: {
+            dev: {
+                src: ['images'],
+            },
+        },
 
-  grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-mkdir');
-  grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images']);
+        /* Generate the images directory if it is missing */
+        mkdir: {
+            dev: {
+                options: {
+                    create: ['images']
+                },
+            },
+        },
+
+    });
+
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images']);
 
 };
